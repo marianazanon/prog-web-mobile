bc22a4bfb4c89bd4b9c7bd4954baa7383778da2a
mariana zanon <100360917+marianazanon@users.noreply.github.com>
2023-09-26 02:26:14 -0300

    Mudança de API

--- Atividades_07/Livros/app.js
+++ Atividades_07/Livros/app.js
@@ -1,45 +1,43 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const getInfoButton = document.getElementById("getInfoButton");
-    getInfoButton.addEventListener("click", getBookInfo);
+// Function to make API request and display results
+function searchBirdSounds() {
+    var query = document.getElementById("search").value;
+    var apiUrl = `https://xeno-canto.org/api/2/recordings?query=${query}`;
 
-    function getBookInfo() {
-        const isbn = "ISBN:0451526538";
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => displayResults(data))
+        .catch(error => {
+            alert("Error fetching data from Xeno-canto API.");
+        });
+}
 
-        
-        const apiUrl = `https://openlibrary.org/api/books?bibkeys=${isbn}&jscmd=data&format=json`;
+// Function to display search results
+function displayResults(data) {
+    var resultsDiv = document.getElementById("results");
+    resultsDiv.innerHTML = "";
 
-    
-        fetch(apiUrl)
-            .then((response) => response.json())
-            .then((data) => {
-                const bookData = data[isbn];
-                displayBookInfo(bookData);
-            })
-            .catch((error) => {
-                console.error("Error fetching book data:", error);
-            });
+    if (data.numRecordings === "0") {
+        resultsDiv.innerHTML = "No results found.";
+        return;
     }
 
-    function displayBookInfo(bookData) {
-        const bookInfoDiv = document.getElementById("bookInfo");
-
-        if (bookData) {
-            
-            const title = bookData.title;
-            const authors = bookData.authors.map((author) => author.name).join(", ");
-            const publishDate = bookData.publish_date;
-            const description = bookData.description;
-
-            const bookInfoHTML = `
-                <h2>${title}</h2>
-                <p><strong>Authors:</strong> ${authors}</p>
-                <p><strong>Publish Date:</strong> ${publishDate}</p>
-                <p><strong>Description:</strong> ${description}</p>
-            `;
-
-            bookInfoDiv.innerHTML = bookInfoHTML;
-        } else {
-            bookInfoDiv.innerHTML = "<p>Book not found.</p>";
-        }
+    var recordings = data.recordings;
+    for (var i = 0; i < recordings.length; i++) {
+        var recording = recordings[i];
+        var recordingDiv = document.createElement("div");
+        recordingDiv.innerHTML = `
+            <h2>${recording.en}</h2>
+            <p>Recorded by: ${recording.rec}</p>
+            <p>Location: ${recording.loc}</p>
+            <p>Date: ${recording.date}</p>
+            <audio controls>
+                <source src="${recording.file}" type="audio/mpeg">
+                Your browser does not support the audio element.
+            </audio>
+        `;
+        resultsDiv.appendChild(recordingDiv);
     }
-});
+}
+
+// Attach event listener to the search button
+document.getElementById("searchButton").addEventListener("click", searchBirdSounds);
